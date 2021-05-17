64db48a6986a734bf5c7fd2de793cc2b22e4b45f
Ranga Reddy <rangareddy.avula@gmail.com>
2021-05-17 21:33:49 +0530

    Update ArraySizeExceedsLimitDemo.java

--- ArraySizeExceedsLimitDemo.java
+++ ArraySizeExceedsLimitDemo.java
@@ -1,3 +1,6 @@
+// ArraySizeExceedsLimitDemo.java
+// JVM Parameters: -Xms10m -Xmx10m
+
 public class ArraySizeExceedsLimitDemo {
     private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE;
     public static void main(String[] args) {
