c755ac7faa382a8c07dbb2a2b6609ad2ae536b38
Ranga Reddy <rangareddy.avula@gmail.com>
2021-05-17 21:36:29 +0530

    Update MetaspaceDemo.java

--- MetaspaceDemo.java
+++ MetaspaceDemo.java
@@ -1,11 +1,14 @@
+// MetaspaceDemo.java
+
 import javassist.CannotCompileException;
 import javassist.ClassPool;
+
+// JVM Parameters: -XX:MetaspaceSize=64M -XX:MaxMetaspaceSize=256M
 
 public class MetaspaceDemo {
     public static void main(String args[]) throws Exception
     {
         ClassPool classPool = ClassPool.getDefault();
-
         for (int i = 0; i <100000000 ; i++) {
             Class claz = classPool.makeClass("MetaspaceDemo" + i).toClass();
         }
