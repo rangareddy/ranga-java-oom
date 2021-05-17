0aeff715cd0e98e86d7915bd9e77dc132c21948d
Ranga Reddy <rangareddy.avula@gmail.com>
2021-05-17 21:34:45 +0530

    Update PermgenSpaceDemo.java

--- PermgenSpaceDemo.java
+++ PermgenSpaceDemo.java
@@ -1,5 +1,6 @@
+// PermgenSpaceDemo.java
 import java.util.*;
-
+// JVM Parameters: -XX:PermSize=2M -XX:MaxPermSize=5M
 public class PermgenSpaceDemo {
     public static void main(String args[]) {
         List<String> list = new ArrayList<>();
