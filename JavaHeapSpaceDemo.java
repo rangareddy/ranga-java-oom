04a68e93827544aff1b4c9d28ad798cf8f8deae4
Ranga Reddy <rangareddy.avula@gmail.com>
2021-05-17 21:15:30 +0530

    Update JavaHeapSpaceDemo.java

--- JavaHeapSpaceDemo.java
+++ JavaHeapSpaceDemo.java
@@ -1,5 +1,5 @@
 import java.util.*;
-
+// JVM params -Xms10m -Xmx10m -XX:+HeapDumpOnOutOfMemoryError
 public class JavaHeapSpaceDemo {
     private final static int SIZE = 1024 * 1024 * 1024;
     public static void main(String[] args) {
