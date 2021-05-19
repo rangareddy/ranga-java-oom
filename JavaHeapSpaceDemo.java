401ee52a8f9753e83c6082631e85973d479d9a4d
Ranga Reddy <rangareddy.avula@gmail.com>
2021-05-19 17:42:24 +0530

    Update JavaHeapSpaceDemo.java

--- JavaHeapSpaceDemo.java
+++ JavaHeapSpaceDemo.java
@@ -1,9 +1,7 @@
 // JavaHeapSpaceDemo.java
-
-import java.util.*;
-
 // JVM Parameters: -Xms10m -Xmx10m
 
+import java.util.*;
 public class JavaHeapSpaceDemo {
     private final static int SIZE = 1024 * 1024 * 1024;
     public static void main(String[] args) {
