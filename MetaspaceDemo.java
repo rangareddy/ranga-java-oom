f06152463c89093f9e3b3b09e98947d0a151b977
Ranga Reddy <rangareddy.avula@gmail.com>
2021-05-19 17:47:22 +0530

    Update MetaspaceDemo.java

--- MetaspaceDemo.java
+++ MetaspaceDemo.java
@@ -1,5 +1,7 @@
 // MetaspaceDemo.java
 // JVM Parameters: -XX:MetaspaceSize=64M -XX:MaxMetaspaceSize=256M
+// javac -cp javassist.jar MetaspaceDemo.java
+// java -cp .:javassist.jar MetaspaceDemo
 
 import javassist.CannotCompileException;
 import javassist.ClassPool;
