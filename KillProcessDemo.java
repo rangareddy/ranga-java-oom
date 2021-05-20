087b8cfaa90bb66dc0fbe5e6deebdff44e8fe545
Ranga Reddy <rangareddy.avula@gmail.com>
2021-05-20 08:31:43 +0530

    Update KillProcessDemo.java

--- KillProcessDemo.java
+++ KillProcessDemo.java
@@ -1,12 +1,9 @@
+// KillProcessDemo.java
 import java.util.*;
 
 public class KillProcessDemo {
     public static void main(String[] args) {
         List<int[]> list = new ArrayList();
-        processKillLoop(list);
-    }
-
-    private static void processKillLoop(List<int[]> list) {
         for ( int i = 10000 ; i < 100000 ; i++) {
             try{
                 list.add(new int[100_000_000]);
